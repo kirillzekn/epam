706149dcecfee7981ed8229708039aa611230224
Kirill Zelentsov <kzelentsov@tengizchevroil.com>
2021-12-23 16:25:30 +0500

    new changes

--- terraform/infrastructure/main.tf
+++ terraform/infrastructure/main.tf
@@ -6,28 +6,31 @@ terraform {
     }
   }
 }
-
+###############################
 # Configure the Azure provider
 provider "azurerm" {
-
   features {}
 }
-
+###############################
+# Data
 data "archive_file" "file_function_app" {
   type        = "zip"
   source_dir  = "../function-app"
   output_path = "function-app.zip"
 }
-
+###############################
+# Locals
 locals {
        publish_code_command = "az webapp deployment source config-zip --resource-group ${azurerm_resource_group.rg.name} --name ${azurerm_function_app.function_app.name} --src ${data.archive_file.file_function_app.output_path}"
 }
-
+###############################
+# Resource Group
 resource "azurerm_resource_group" "rg" {
   name     = "${var.project}-rg"
   location = var.location
 }
-
+###############################
+# App service
 resource "azurerm_app_service_plan" "app_service_plan" {
   name                = "${var.project}-app-service-plan"
   resource_group_name = azurerm_resource_group.rg.name
@@ -40,11 +43,9 @@ resource "azurerm_app_service_plan" "app_service_plan" {
     size = "Y1"
   }
 }
-
-
-
+###############################
+# Function
 resource "azurerm_function_app" "function_app" {
-  
   name                       = "${var.project}-function-app"
   resource_group_name        = azurerm_resource_group.rg.name
   location                   = var.location
@@ -58,22 +59,15 @@ resource "azurerm_function_app" "function_app" {
   os_type = "linux"
   site_config {
     linux_fx_version          = "python|3.7"
-    #use_32_bit_worker_process = true
   }
   storage_account_name       = var.storage_account_name
   storage_account_access_key = var.storage_account_access_key
   version                    = "~3"
 
-  # lifecycle {
-  #   ignore_changes = [
-  #     app_settings["WEBSITE_RUN_FROM_PACKAGE"],
-  #   ]
-  # }
 }
-
-
+###############################
+# Null resource for Function
 resource "null_resource" "function_app_publish" {
-
   provisioner "local-exec" {
     command = local.publish_code_command
   }
@@ -82,4 +76,42 @@ resource "null_resource" "function_app_publish" {
     input_json = filemd5(data.archive_file.file_function_app.output_path)
     publish_code_command = local.publish_code_command
   }
+}
+###############################
+# SQL server 
+resource "azurerm_mysql_server" "mysql-server" {
+  name = "${var.project}-mysql-server"
+  location = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+ 
+  administrator_login = var.mysql-admin-login
+  administrator_login_password = var.mysql-admin-password
+ 
+  sku_name = var.mysql-sku-name
+  version = var.mysql-version
+ 
+  storage_mb = var.mysql-storage
+  auto_grow_enabled = true
+  
+  backup_retention_days = 7
+  geo_redundant_backup_enabled = false
+  public_network_access_enabled = true
+  ssl_enforcement_enabled = true
+  ssl_minimal_tls_version_enforced = "TLS1_2"
+}
+###############################
+# SQL DB
+resource "azurerm_mysql_database" "mysql-db" {
+  name                = "${var.project}-mysql-db"
+  resource_group_name = azurerm_resource_group.rg.name
+  server_name         = azurerm_mysql_server.mysql-server.name
+  charset             = "utf8"
+  collation           = "utf8_unicode_ci"
+}
+###############################
+# Static Web App
+resource "azurerm_static_site" "my_webapp" {
+  name                = "${var.project}-static-webapp"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
 }
--- terraform/infrastructure/variables.tf
+++ terraform/infrastructure/variables.tf
@@ -25,20 +25,20 @@ variable "mysql-admin-password" {
   type = string
   description = "Password to authenticate to MySQL Server"
 }
-# variable "mysql-version" {
-#   type = string
-#   description = "MySQL Server version to deploy"
-#   default = "8.0"
-# }
-# variable "mysql-sku-name" {
-#   type = string
-#   description = "MySQL SKU Name"
-#   default = "8.0"
-# }
-# variable "mysql-storage" {
-#   type = string
-#   description = "MySQL Storage in MB"
-#   default = "5120"
-# }
+variable "mysql-version" {
+  type = string
+  description = "MySQL Server version to deploy"
+  default = "8.0"
+}
+variable "mysql-sku-name" {
+  type = string
+  description = "MySQL SKU Name"
+  default = "8.0"
+}
+variable "mysql-storage" {
+  type = string
+  description = "MySQL Storage in MB"
+  default = "5120"
+}
 
 
