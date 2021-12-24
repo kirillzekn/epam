7956eeb717b2181c8a48e0410da1bfd7a29fd6e3
Kirill Zelentsov <kzelentsov@tengizchevroil.com>
2021-12-24 15:26:08 +0500

    commit change

--- terraform/infrastructure/main.tf
+++ terraform/infrastructure/main.tf
@@ -115,3 +115,13 @@ resource "azurerm_static_site" "my_webapp" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
 }
+
+###############################
+# Blob Storage
+# resource "azurerm_storage_blob" "storage_blob" {
+#   name = "${filesha256(var.archive_file.output_path)}.zip"
+#   storage_account_name = azurerm_storage_account.storage_account.name
+#   storage_container_name = azurerm_storage_container.storage_container.name
+#   type = "Block"
+#   source = var.archive_file.output_path
+# }
