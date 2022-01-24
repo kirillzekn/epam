8af6c77288c44dbea5f3e13a1374513f64c726f8
kirillzekn <47919858+kirillzekn@users.noreply.github.com>
2022-01-24 14:18:48 +0500

    Update __init__.py

--- terraform/infrastructure/function-app/__init__.py
+++ terraform/infrastructure/function-app/__init__.py
@@ -1,3 +1,27 @@
-def main(req):
-    user = req.params.get('user')
-    return f'Hello, {user}!'
+#def main(req):
+#    user = req.params.get('user')
+#    return f'Hello, {user}!'
+
+import logging
+import azure.functions as func
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        return func.HttpResponse(f"Hello {name}!")
+    else:
+        return func.HttpResponse(
+            "Please pass a name on the query string or in the request body",
+            status_code=400
+        )
